c50a1feddb241a50476fd57bc9e0323596727052
Ivan Gasques <ivanngasques@gmail.com>
2022-07-02 12:26:54 +0100

    Comentarios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import {Browser as Router, Switch, Route, link} from 'react-router-dom'
 import Menu from "./components/Menu/Menu";
 import Card from "./components/Card/Card";
 import Carrosel from "./components/Carousel/Carrosel";
 import Comentario from "./components/Comentarios/Comentarios";
 import Footer from "./components/Footer/Footer";
+import './App.css'
 
 function App() {
   return (
